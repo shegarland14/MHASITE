e526a855500f79a5079cc3576fc8e1df0dfa45b4
Sheridan Garland <shegarland14@gmail.com>
2023-01-14 11:35:52 -0500

    updated steps/ added icons

--- src/sell.js
+++ src/sell.js
@@ -6,13 +6,32 @@ export default function Sell(){
         <div className="sell">
             <h1> WANT TO SELL YOUR MOBILE HOME?</h1>
             <h2> FOLLOW THESE 3 THREE STEPS</h2>
+
         <div className="steps">
-            <button> STEP 1</button>
-            <button> STEP 2</button> 
-            <button> STEP 3</button>    
-   
-    
-         </div>
+            <div className="col1"> 
+             <button clasName="button1"> STEP 1</button>
+                <h3>CONTACT US <br/> 803 848 7472</h3>
+                <i  class="fa fa-volume-control-phone fa-3x" aria-hidden="true"></i>
+             </div>
+
+            <div className="col2"> 
+            <button className="button2"> STEP 2</button>
+                <h3>GET OFFER</h3>
+                <i class="fa fa-handshake-o fa-3x" aria-hidden="true"></i>
+
+            </div>
+
+            <div className="col3"> 
+                <button className="button3"> STEP 3</button> 
+                <h3>GET CASH</h3>
+                <i class="fa fa-money fa-3x" aria-hidden="true"></i>
+
+            </div>
+            </div>
+
+        
+
+        
         </div>
 
     )
