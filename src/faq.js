6f765b4244fb1ae33d5ff169ee928da4dcdc7967
Sheridan Garland <shegarland14@gmail.com>
2023-01-16 23:50:58 -0500

    updates to faq

--- src/faq.js
+++ src/faq.js
@@ -9,13 +9,15 @@ export default function Faq(){
                      When will the property sell? How much will I get? 
                      What will it cost me? <br/>Below, we offer answers to some of our most frequently asked questions. 
                     <br/>If there is anything else you would like to know, do not be afraid to reach out!<br/> (803) 848-7472</h2>
-                <h2>I was thinking about making some upgrades to my home. ShouldI do that 
-                    before you come and see it?
-                </h2>
-                <h2>I am selling my mobile home, but NOT my land.
+                <div className="questions">                
+                <h3><span>Q:</span>  I was thinking about making some upgrades to my home.<br/>
+                     Should I do that before you come and see it?
+                </h3><br/>
+                <h3><span>Q:</span>  I am selling my mobile home, but NOT my land.<br/>
                     Do you guys buy homes that need to be moved too?
-                </h2>
-                <h2>Do you only buy junk mobile homes?</h2>
-                <h2>How soon can you make me an offer?</h2>
+                </h3><br/>
+                <h3><span>Q:</span>  Do you only buy junk mobile homes?</h3><br/>
+                <h3><span>Q:</span>  How soon can you make me an offer?</h3><br/>
+                </div> 
         </div>
     )}
