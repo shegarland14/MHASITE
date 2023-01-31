de5156995eb8148e3e96876a1dd06381451d6c09
Sheridan Garland <shegarland14@gmail.com>
2023-01-31 12:40:29 -0500

    netlify.toml

--- src/form.js
+++ src/form.js
@@ -17,18 +17,16 @@ export default function Form() {
     const handleChange = (e) => {
         const name = e.target.name;
         const value = e.target.value;
-
         setDetails((prev) => {
             return {...prev, [name]: value}
         });
     };
+
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(details)
-
     };
-
-
+    
  return (
     <div className="form-container">
         <form onSubmit = {handleSubmit}>               
@@ -37,62 +35,16 @@ export default function Form() {
             </div>
         
             <div className="form-body">    
-                <input 
-                    type="text"
-                    name = 'name'
-                    placeholder='Enter Your Name'
-                    onChange ={handleChange}
-                    />
-            
-            
-                <input 
-                    type="text"
-                    name = 'email'
-                    placeholder='Enter Your Email'
-                     onChange ={handleChange}
+                <input type="text" name = 'name' placeholder='Name' onChange ={handleChange}/>
+                <input type="text" name = 'email' placeholder='Enter Your Email'onChange ={handleChange}/>
+                <input type="text" name = 'number' placeholder='Enter Your Number'onChange ={handleChange}/>
+                <input type="text" name = 'year' placeholder='Year'onChange ={handleChange}/>
+                <input type="text" name = 'beds' placeholder='bed(s)'onChange ={handleChange}/>
+                <input type="text" name = 'baths' placeholder='bath(s)'onChange ={handleChange}/>
+                <input type="text" name = 'address' placeholder='address'onChange ={handleChange}/>
+                <input type="text" name = 'moved' placeholder='Needs to be Moved?'onChange ={handleChange}/>
 
-                    />
-            
-            
-                <input 
-                    type="text"
-                    name='number'
-                    placeholder='Phone Number'
-                    onChange ={handleChange}
-                    />
-             
-            
-                <input 
-                    type="text"
-                    name = 'year'
-                    placeholder='Year'
-                    onChange ={handleChange}
-                />
-            
-                <input 
-                    type="text"
-                    name= 'bed'
-                    placeholder='Bed(s)'
-                    onChange ={handleChange}
-                />
-            <input 
-                    type="text"
-                    name = 'baths'
-                    placeholder='Bath(s)'
-                    onChange ={handleChange}
-                />
-                <input 
-                    type="text"
-                    name = 'address'
-                    placeholder='Property Address'
-                    onChange ={handleChange}
-                />
-                <input 
-                    type="text"
-                    name = 'moved'
-                    placeholder='Needs To Be Moved?'
-                    onChange ={handleChange}
-                />
+
                </div>
             <div class="form-footer"> 
                 <a href="/"> 
